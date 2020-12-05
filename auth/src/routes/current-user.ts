cca40770448c37d43721835b2c8d1469621057a2
faiyuching <faiyuching@gmail.com>
2020-12-05 16:18:42 +0800

    update currentuser.js

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -5,6 +5,7 @@ const router = express.Router();
 
 router.get('/api/users/currentuser', currentUser, (req, res) => {
   res.send({ currentUser: req.currentUser || null });
+  res.end()
 });
 
 export { router as currentUserRouter };
